dae3665e05d76e297b3cf91442d2ae86e41c1bdc
iwiniwin <iwiniwin@foxmail.com>
2020-09-08 20:21:46 +0800

    添加游戏挡板

--- src/core/game.cc
+++ src/core/game.cc
@@ -3,9 +3,15 @@
 #include "game.h"
 #include "resource_manager.h"
 #include "../render/sprite_renderer.h"
-
+#include "GLFW/glfw3.h"
 
 SpriteRenderer* sprite_renderer;
+
+// 挡板大小
+const glm::vec2 kPaddleSize(100, 20);
+// 挡板速度
+const float kPaddleVelocity(500.0f);
+GameObject* paddle;
 
 Game::Game(unsigned int width, unsigned int height){
     width_ = width;
@@ -30,9 +36,10 @@ void Game::Init(){
 
     // 加载纹理
     ResourceManager::LoadTexture("resources/textures/background.jpg", false, "background");
-    ResourceManager::LoadTexture("resources/textures/awesomeface.png", GL_TRUE, "face");
-    ResourceManager::LoadTexture("resources/textures/block_solid.png", GL_TRUE, "block_solid");
-    ResourceManager::LoadTexture("resources/textures/block.png", GL_TRUE, "block");
+    ResourceManager::LoadTexture("resources/textures/awesomeface.png", true, "face");
+    ResourceManager::LoadTexture("resources/textures/block_solid.png", false, "block_solid");
+    ResourceManager::LoadTexture("resources/textures/block.png", false, "block");
+    ResourceManager::LoadTexture("resources/textures/paddle.png", true, "paddle");
 
     // 加载关卡
     GameLevel one;
@@ -53,6 +60,10 @@ void Game::Init(){
 
     // 设置当前关卡
     level_ = 0;
+
+    // 初始化挡板
+    glm::vec2 paddle_pos = glm::vec2(width_ / 2 - kPaddleSize.x / 2, height_ - kPaddleSize.y);
+    paddle = new GameObject(paddle_pos, kPaddleSize, ResourceManager::GetTexture("paddle"));
 }
 
 void Game::Update(float dt){
@@ -60,7 +71,22 @@ void Game::Update(float dt){
 }
 
 void Game::ProcessInput(float dt){
-
+    if(state_ == KGameActive){
+        float velocity = kPaddleVelocity * dt;
+        // 移动挡板
+        if(keys_[GLFW_KEY_A]){
+            paddle->position_.x -= velocity;
+            if(paddle->position_.x < 0)
+                paddle->position_.x = 0;
+        }
+            
+        if(keys_[GLFW_KEY_D]){
+            paddle->position_.x += velocity;
+            if(paddle->position_.x > width_ - paddle->size_.x)
+                paddle->position_.x = width_ - paddle->size_.x;
+        }
+            
+    }
 }
 
 void Game::Render(){
@@ -71,6 +97,9 @@ void Game::Render(){
 
         // 绘制关卡
         levels_[level_].Draw(*sprite_renderer);
+
+        // 绘制挡板
+        paddle->Draw(*sprite_renderer);
     }
 }
 
